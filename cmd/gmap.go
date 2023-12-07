741c89a25247a40f5f5de4ddac9cb14d9110bbeb
xxxxnnxxxx <xxxxnnxxxx@126.com>
2023-12-07 18:27:07 +0800

    增加ping探测控制参数

--- cmd/gmap.go
+++ cmd/gmap.go
@@ -27,24 +27,24 @@ func main() {
 	var isDectivePing bool // 执行ping
 	var outputpath string
 
-	flag.BoolVar(&sS, "sS", false, "SYN port scan")
-	flag.BoolVar(&sT, "sT", false, "TCP connect port scan")
-	flag.BoolVar(&sU, "sU", false, "UDP port scan")
-	flag.BoolVar(&isDectivePing, "sN", false, "Ping detective hosts")
-	flag.BoolVar(&isSrvDetection, "sV", false, "Service Detection")
-	flag.StringVar(&szport, "p", "", "port list")
-	flag.StringVar(&target, "t", "", "input a target, formats: 192.168.1.1/24 or 192.168.1.1,192.168.1.2 or 192.168.1.1")
-	flag.Float64Var(&timeout, "to", 2, "timeout per probe (seconds")
+	flag.BoolVar(&sS, "sS", false, "SYN扫描")
+	flag.BoolVar(&sT, "sT", false, "TCP连接扫描")
+	flag.BoolVar(&sU, "sU", false, "UDP扫描")
+	flag.BoolVar(&isDectivePing, "sN", false, "ping探活")
+	flag.BoolVar(&isSrvDetection, "sV", false, "服务探测")
+	flag.StringVar(&szport, "p", "", "指定扫描端口列表")
+	flag.StringVar(&target, "t", "", "输入扫描目标, 格式: 192.168.1.1/24 or 192.168.1.1,192.168.1.2 or 192.168.1.1")
+	flag.Float64Var(&timeout, "to", 2, "超时时间（秒）")
 	flag.BoolVar(&c1, "c1", true, "try 1")
 	flag.BoolVar(&c2, "c2", false, "try 2")
 	flag.BoolVar(&c3, "c3", false, "try 3")
 	flag.BoolVar(&c4, "c4", false, "try 4")
 	flag.BoolVar(&c5, "c5", false, "try 5")
-	flag.BoolVar(&arp, "arp", false, "print arp table")
-	flag.BoolVar(&route, "route", false, "print route table")
-	flag.UintVar(&ifindex, "if", 0, "a interface index for the scanner")
-	flag.BoolVar(&pif, "pif", false, "print interface info")
-	flag.StringVar(&outputpath, "o", "", "output json file")
+	flag.BoolVar(&arp, "arp", false, "打印arp地址表")
+	flag.BoolVar(&route, "route", false, "打印路由表")
+	flag.UintVar(&ifindex, "if", 0, "指定扫描所经过的网络接口")
+	flag.BoolVar(&pif, "pif", false, "打印网络接口信息")
+	flag.StringVar(&outputpath, "o", "", "输出到json文件")
 
 	flag.Parse()
 
@@ -68,7 +68,7 @@ func main() {
 
 	probeManager := manage.NewProbeManager()
 	if len(common.TrimEx(target)) == 0 || len(common.TrimEx(szport)) == 0 {
-		log.Logger.Error("please a target and ports")
+		log.Logger.Error("请输入目标和端口")
 		os.Exit(1)
 	}
 
@@ -106,7 +106,7 @@ func main() {
 	}
 
 	if sS && sT && sU == false {
-		log.Logger.Error("please select a scan type.")
+		log.Logger.Error("请选择一个扫描类型(SYN/TCP connect)")
 		os.Exit(1)
 	}
 
@@ -124,7 +124,7 @@ func main() {
 	// 分析targets
 	ips := common.GetIPsFromString(target)
 	if len(ips) == 0 {
-		log.Logger.Error("please input a vaild target")
+		log.Logger.Error("请输入一个有效的目标")
 		os.Exit(1)
 	}
 	// 指定接口索引
@@ -135,6 +135,18 @@ func main() {
 	probeManager.Do()
 	//
 	probeManager.Wait()
+
+	if len(probeManager.OutputPath) > 0 {
+		r2j, err := probeManager.Result2JSON()
+		if err != nil {
+			log.Logger.Error(err)
+		}
+
+		_, err = common.WriteFile(probeManager.OutputPath, []byte(r2j))
+		if err != nil {
+			log.Logger.Error(err)
+		}
+	}
 
 	// 打印结果
 	probeManager.PrintResult()
