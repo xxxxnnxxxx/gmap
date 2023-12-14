1bc0bec2828193e1c2b3ee0601fa60147beea08f
xxxxnnxxxx <xxxxnnxxxx@126.com>
2023-12-14 23:26:35 +0800

    修改成和nmap参数一样的输入方式

--- cmd/gmap.go
+++ cmd/gmap.go
@@ -6,8 +6,8 @@ import (
 	"Gmap/gmap/manage"
 	"Gmap/gmap/manage/scanner"
 	"Gmap/gmap/netex/device"
-	"flag"
 	"github.com/felixge/fgprof"
+	opt "github.com/pborman/getopt/v2"
 	"net/http"
 	_ "net/http/pprof"
 	"os"
@@ -19,107 +19,111 @@ func monitor() {
 
 }
 func main() {
-	var target string
-	var isSrvProbe bool
 	var szport string
-	var sS, sT bool
-	// var timeout float64
-	var arp, route bool
-	var ifindex int // 指定索引
-	var pif bool
-	var isPingtestNo bool // 执行ping
-	var outputpath string
 
-	flag.BoolVar(&sS, "sS", false, "SYN扫描")
-	flag.BoolVar(&sT, "sT", false, "TCP连接扫描")
-	flag.BoolVar(&isPingtestNo, "Pn", false, "禁止ping探活")
-	flag.BoolVar(&isSrvProbe, "sV", false, "服务探测")
-	flag.StringVar(&szport, "p", "", "指定扫描端口列表")
-	flag.StringVar(&target, "t", "", "输入扫描目标, 格式: 192.168.1.1/24 or 192.168.1.1,192.168.1.2 or 192.168.1.1")
-	// flag.Float64Var(&timeout, "to", 2, "超时时间（秒）")
-	flag.BoolVar(&arp, "arp", false, "打印arp地址表")
-	flag.BoolVar(&route, "route", false, "打印路由表")
-	flag.IntVar(&ifindex, "if", -1, "指定扫描所使用的网络出口")
-	flag.BoolVar(&pif, "pif", false, "打印网络接口信息")
-	flag.StringVar(&outputpath, "o", "", "输出到json文件")
+	s := opt.Bool('s', "端口扫描类")
+	S := opt.Bool('S', "SYN 扫描")
+	T := opt.Bool('T', "Connect 连接扫描")
+	V := opt.Bool('V', "探测服务版本")
+	P := opt.Bool('P', "ping 探活")
+	n := opt.Bool('n', "否定操作")
+	port := opt.String('p', "", "指定端口列表")
+	printArp := opt.BoolLong("print-arp", 'a', "", "打印arp列表")
+	printRoute := opt.BoolLong("print-route", 'r', "", "打印路由表")
+	ifindex := opt.IntLong("interface-index", 'i', -1, "指定网络接口索引")
+	printInterface := opt.BoolLong("print-interface", 'e', "", "打印网络接口信息")
+	outputpath := opt.StringLong("output", 'o', "", "指定输出文件路径")
 
-	flag.Parse()
+	opt.Parse()
+
+	szport = *port
 
 	// go monitor()
 
-	if arp == true {
+	if *printArp == true {
 		device.DeviceGlobalInit()
 		device.PrintArpTables()
 		return
 	}
 
-	if route == true {
+	if *printRoute == true {
 		device.DeviceGlobalInit()
 		device.PrintRouteTables()
 		return
 	}
 
-	if pif == true {
+	if *printInterface == true {
 		device.DeviceGlobalInit()
 		device.PrintInterfaceInfo()
 		return
 	}
 
 	probeManager := manage.NewProbeManager()
-	if len(common.TrimEx(target)) == 0 || len(common.TrimEx(szport)) == 0 {
-		log.Logger.Error("请输入目标和端口")
+	if len(opt.Args()) == 0 {
+		log.Logger.Error("请输入目标")
+		os.Exit(1)
+	}
+
+	if len(common.TrimEx(szport)) == 0 {
+		log.Logger.Error("请输入要扫描的端口列表")
 		os.Exit(1)
 	}
 
 	// 保存端口和目标的参数
-	probeManager.ArgumentTarget = target
+	probeManager.ArgumentTarget = opt.Args()[0]
 	probeManager.ArgumentPorts = szport
 
 	probeManager.PrintBanner()
 
-	// 超时时间
-	// probeManager.Timeout = timeout
-	// Port scan
-	if sS {
-		probeManager.ScanType = scanner.ScanType_Syn
-	}
-
-	if sT {
-		probeManager.ScanType = scanner.ScanType_TCPConn
-	}
-
-	//if sU {
-	//	probeManager.ScanType = scanner.ScanType_UDP
-	//}
-
-	if sS == false && sT == false {
-		log.Logger.Error("请选择一个扫描类型(SYN/TCP connect)")
+	if *s {
+		if *S == false && *T == false {
+			log.Logger.Error("请选择一个扫描类型(SYN/TCP connect)")
+			os.Exit(1)
+		}
+	} else {
+		log.Logger.Error("请指定sS/sT端口扫描方式")
 		os.Exit(1)
 	}
 
+	// Port scan
+	if *s && *S {
+		probeManager.ScanType = scanner.ScanType_Syn
+	}
+
+	if *s && *T {
+		probeManager.ScanType = scanner.ScanType_TCPConn
+	}
+
 	// 加载服务探测
-	if isSrvProbe {
-		probeManager.IsSrvProbe = isSrvProbe
+	if *s && *V {
+		probeManager.IsSrvProbe = true
+	} else {
+		probeManager.IsSrvProbe = false
 	}
 
 	// 输出路径
-	if len(outputpath) > 0 {
-		probeManager.OutputPath = outputpath
+	if len(*outputpath) > 0 {
+		probeManager.OutputPath = *outputpath
 	}
 
 	// 是否ping测试
-	probeManager.IsPingTest = !isPingtestNo
+	if *P && *n {
+		probeManager.IsPingTest = false
+	} else {
+		probeManager.IsPingTest = true
+	}
+
 	// 端口
 	probeManager.Ports = append(probeManager.Ports, common.Splite_Port(szport)...)
 	// 分析targets
-	ips := common.GetIPsFromString(target)
+	ips := common.GetIPsFromString(opt.Args()[0])
 	if len(ips) == 0 {
 		log.Logger.Error("请输入一个有效的目标")
 		os.Exit(1)
 	}
 	// 指定接口索引
 
-	probeManager.Ifindex = ifindex
+	probeManager.Ifindex = *ifindex
 	// 初始化
 	probeManager.Initialize(ips)
 	// 执行
